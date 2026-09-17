a8b887792a473c5c7a9472fc969d65f922561ec2
ABIRAMI SAKTHIVEL <52281389+abisakthi270599@users.noreply.github.com>
2026-09-17 11:44:49 +0530

    Add files via upload

---
+++ tests/Week2/Day3/salesForceLogin.spec.ts
@@ -0,0 +1,12 @@
+import{test, chromium, expect} from "@playwright/test"
+
+test('Create Lead', async ({page})=> {
+
+    await page.goto("https://login.salesforce.com/?locale=in");
+    await expect(page).toHaveTitle("Login | Salesforce");
+    await page.locator("#username").fill("dilipkumar.rajendran@testleaf.com");
+    await page.locator("#Login").click();
+    await page.locator("#password").fill("TestLeaf@2025");
+    await page.locator("#Login").click();
+   
+});
